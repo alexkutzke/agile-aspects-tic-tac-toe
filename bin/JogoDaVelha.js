343aa5f774476723ab87ce3fd1b1fc22f2295e54
Raphael Oliveira <raphael.oliveira.ros@gmail.com>
2023-09-23 15:05:54 -0300

    tentando arrumar os teste ainda

--- bin/JogoDaVelha.js
+++ bin/JogoDaVelha.js
@@ -1,57 +1,99 @@
-var tabuleiro = [
-  ['', '', ''],
-  ['', '', ''],
-  ['', '', '']
-];
-
-let jogadorAtual = 'X';
-
-export function fazerJogada(linha, coluna) {
-  if (tabuleiro[linha][coluna] === '') {
-    tabuleiro[linha][coluna] = jogadorAtual;
+class JogoDaVelha {
+  
+  constructor() {
+    this.tabuleiro = [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""]
+    ];
     
-    // Verificar vitória após cada jogada
-    if (verificarVencedor(jogadorAtual)) {
-      console.log(`Jogador ${jogadorAtual} venceu!`);
-      reiniciarJogo();
-    } else {
-      jogadorAtual = jogadorAtual === 'X' ? 'O' : 'X';
-    }
+    this.jogadorAtual = "X";
   }
+  
+  reiniciarJogo() {
+    this.tabuleiro == [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""]
+    ];
+    this.jogadorAtual = "X";
+  }
+
+
+    fazerJogada(linha, coluna) {
+      if (this.tabuleiro[linha][coluna] === "") {
+        this.tabuleiro[linha][coluna] = this.jogadorAtual;
+        if (this.jogadorAtual === "X") {
+          this.jogadorAtual = "O";
+        } else {
+          this.jogadorAtual = "X";
+        }
+        
+        console.log("Tabuleiro após a jogada:");
+        this.mostrarTabuleiro();
+      }
+    };
+    
+  
+    mostrarTabuleiro() {
+      for (let i = 0; i < 3; i++) {
+        console.log(this.tabuleiro[i].join(" | "));
+        if (i < 2) {
+          console.log("---------");
+        }
+      }
+  };
+
 }
 
-export function verificarVencedor(jogador) {
-  // Verificar linhas
-  for (let i = 0; i < 3; i++) {
-    if (tabuleiro[i][0] === jogador && tabuleiro[i][1] === jogador && tabuleiro[i][2] === jogador) {
-      return true;
-    }
-  }
+export default JogoDaVelha;
+      
+      // Verificar vitória após cada jogada
+      // if (verificarVencedor(jogadorAtual)) {
+      //   console.log(`Jogador ${jogadorAtual} venceu!`);
+      //   reiniciarJogo();
+      // } else {
+      //   jogadorAtual = jogadorAtual === "X" ? "O" : "X";
+      // }
 
-  // Verificar colunas
-  for (let j = 0; j < 3; j++) {
-    if (tabuleiro[0][j] === jogador && tabuleiro[1][j] === jogador && tabuleiro[2][j] === jogador) {
-      return true;
-    }
-  }
+  
+  //   verificarVencedor(jogador) {
+  //   // Verificar linhas
+  //   for (let i = 0; i < 3; i++) {
+  //     if (this.tabuleiro[i][0] === jogador &&
+  //         this.tabuleiro[i][1] === jogador &&
+  //         this.tabuleiro[i][2] === jogador) {
+  //       return true;
+  //     }
+  //   }
+  //   return false; // Nenhum vencedor encontrado
+  // }
 
-  // Verificar diagonais
-  if (tabuleiro[0][0] === jogador && tabuleiro[1][1] === jogador && tabuleiro[2][2] === jogador) {
-    return true;
-  }
-  if (tabuleiro[0][2] === jogador && tabuleiro[1][1] === jogador && tabuleiro[2][0] === jogador) {
-    return true;
-  }
-
-  return false; // Nenhum vencedor encontrado
-}
-
-
-export function reiniciarJogo() {
-  tabuleiro = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-  ];
-  jogadorAtual = 'X';
-}
+  
+  // verificarVencedor(jogador) {
+    // Verificar linhas
+    // for (let i = 0; i < 3; i++) {
+    //   if (tabuleiro[i][0] == jogador && tabuleiro[i][1] == jogador && tabuleiro[i][2] === jogador) {
+    //     return true
+    //   }
+    // }
+  
+    // // Verificar colunas
+    // for (let j = 0; j < 3; j++) {
+    //   if (tabuleiro[0][j] === jogador && tabuleiro[1][j] === jogador && tabuleiro[2][j] === jogador) {
+    //     return true;
+    //   }
+    // }
+  
+    // Verificar diagonais
+  //   if (tabuleiro[0][0] == jogador && tabuleiro[1][1] == jogador && tabuleiro[2][2] == jogador) {
+  //     return true
+  //   }
+  //   if (tabuleiro[0][2] == jogador && tabuleiro[1][1] == jogador && tabuleiro[2][0] == jogador) {
+  //     return true
+  //   }
+  
+  //   return false; // Nenhum vencedor encontrado
+  // }
+  
+  
--- bin/JogoDaVelha.test.js
+++ bin/JogoDaVelha.test.js
@@ -1,25 +1,81 @@
-const { fazerJogada, verificarVencedor, reiniciarJogo } = require('./JogoDaVelha.js');
+import JogoDaVelha from "./JogoDaVelha";
 
-test('Fazer uma jogada', () => {
-  reiniciarJogo();
-  fazerJogada(0, 0); // Primeira jogada, jogador 'X'
-  expect(verificarVencedor('X')).toBe(false); // Não deve haver vencedor ainda
-});
+describe('Jogo da velha', () => {
+  describe('Verificar Jogo Novo', () => {
+    test('Testes para ver se o Jogo está sendo criado', () => {
+      const jogo = new JogoDaVelha();
+      console.log(`Jogador Atual: ${jogo.jogadorAtual}`);
+      console.log(jogo.tabuleiro)
+      expect(jogo.tabuleiro).toBe([
+        ['', '', ''], ['', '', ''], ['', '', '']]);
+    });
 
-test('Verificar vitória na 1ª linha', () => {
-  reiniciarJogo();
-  fazerJogada(0, 0); // 'X'
-  fazerJogada(1, 0); // 'O'
-  fazerJogada(0, 1); // 'X'
-  fazerJogada(1, 1); // 'O'
-  fazerJogada(0, 2); // 'X' (vitória na linha 0)
-  expect(verificarVencedor('X')).toBe(true);
+    test('Testes para ver se jogadas são feitas', () => {
+      const jogo = new JogoDaVelha();
+      jogo.reiniciarJogo();
+      jogo.fazerJogada(0, 0); // "X"
+      expect(jogo.tabuleiro[0][0]).toBe("X");
+    });
+
+  });
 });
+    
+//     test('Testes para o método de verificar vencedor linha 1', () => {
+//       const jogo = new JogoDaVelha();
+//       jogo.reiniciarJogo();
+//       jogo.fazerJogada(0, 0); // 'X'
+//       jogo.fazerJogada(0, 1); // 'X'
+//       jogo.fazerJogada(0, 2); // 'X'
+//       expect(jogo.verificarVencedor("X")).toBe(true);
+//     });
+//   });
+// });
+
+
+
+// test('Fazer uma jogada', () => {
+//   reiniciarJogo();
+//   fazerJogada(0, 0); // Primeira jogada, jogador 'X'
+//   expect(verificarVencedor('X')).toBe(false); // Não deve haver vencedor ainda
+// });
+
+
+
+// test('Verificar vitória na diagonal 1', () => {
+//   reiniciarJogo();
+//   fazerJogada(0, 0); // 'X'
+//   fazerJogada(0, 1); // 'O'
+//   fazerJogada(1, 1); // 'X'
+//   fazerJogada(0, 2); // 'O'
+//   fazerJogada(2, 2); // 'X' (vitória na diagonal 1)
+//   console.log(tabuleiro)
+//   expect(verificarVencedor("X")).toBe(true);
+// });
+
+// test('Verificar vitória na diagonal 2', () => {
+//   reiniciarJogo();
+//   fazerJogada(2, 0); // 'X'
+//   fazerJogada(0, 0); // 'O'
+//   fazerJogada(1, 1); // 'X'
+//   fazerJogada(0, 1); // 'O'
+//   fazerJogada(0, 2); // 'X' (vitória na diagonal 2)
+//   expect(verificarVencedor('X')).toBe(true);
+// });
+
+// test('Verificar vitória na 1ª linha', () => {
+  //   reiniciarJogo();
+  //   fazerJogada(0, 0); // 'X'
+  //   fazerJogada(1, 0); // 'O'
+  //   fazerJogada(0, 1); // 'X'
+  //   fazerJogada(1, 1); // 'O'
+  //   fazerJogada(0, 2); // 'X' (vitória na linha 0)
+  //   expect(verificarVencedor('X')).toBe(true);
+  // });
 
 // test('Verificar vitória na 2ª linha', () => {
-//   reiniciarJogo();
-//   fazerJogada(1, 0); // 'X'
-//   fazerJogada(0, 0); // 'O'
+  //   reiniciarJogo();
+  //   fazerJogada(1, 0); // 'X'
+  //   fazerJogada(0, 0); // 'O'
 //   fazerJogada(1, 1); // 'X'
 //   fazerJogada(0, 1); // 'O'
 //   fazerJogada(1, 2); // 'X' (vitória na linha 1)
@@ -66,25 +122,6 @@ test('Verificar vitória na 1ª linha', () => {
 //   expect(verificarVencedor()).toBe('X');
 // });
 
-// test('Verificar vitória na diagonal 1', () => {
-//   reiniciarJogo();
-//   fazerJogada(0, 0); // 'X'
-//   fazerJogada(0, 1); // 'O'
-//   fazerJogada(1, 1); // 'X'
-//   fazerJogada(0, 2); // 'O'
-//   fazerJogada(2, 2); // 'X' (vitória na diagonal 1)
-//   expect(verificarVencedor()).toBe('X');
-// });
-
-// test('Verificar vitória na diagonal 2', () => {
-//   reiniciarJogo();
-//   fazerJogada(2, 0); // 'X'
-//   fazerJogada(0, 0); // 'O'
-//   fazerJogada(1, 1); // 'X'
-//   fazerJogada(0, 1); // 'O'
-//   fazerJogada(0, 2); // 'X' (vitória na diagonal 2)
-//   expect(verificarVencedor()).toBe('X');
-// });
 
 
 // test('Verificar empate', () => {
