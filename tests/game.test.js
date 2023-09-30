008f947e6d6e1452d6a9730c72c2c7e88f18b081
tozamba <tozamba@hotmail.com>
2023-09-29 21:30:04 -0300

    Update game.test.js

--- tests/game.test.js
+++ tests/game.test.js
@@ -1,1 +1,17 @@
+// tests/game.test.js
+const TicTacToe = require('../src/game');
+
+describe('TicTacToe', () => {
+  it('deve criar um novo jogo vazio', () => {
+    const game = new TicTacToe();
+    expect(game.board).toEqual([
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
+    ]);
+    expect(game.currentPlayer).toBe('X');
+    expect(game.winner).toBe(null);
+    expect(game.isDraw()).toBe(false);
+  });
+});
 
